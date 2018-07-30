2204cc607abe4a0e29dc36c87cabd28d1d738c04
eunji120 <eunji120@gmail.com>
2018-07-29 20:23:13 -0400

    Added omdb API

--- keys.js
+++ keys.js
@@ -2,6 +2,8 @@ js
 console.log('this is loaded');
 
 exports.spotify = {
-    id: process.env.SPOTIFY_ID,
+    id: process.env.c98373ed65ac4b189e7da544fcab22e2,
     secret: process.env.SPOTIFY_SECRET
   };
+
+  exports.omdbKey = '1f8a2586';
